3a2adc9855719b7a9df415bfaf0fbfa1e6f7dc24
guopeining <guopeining@baijiahulian.com>
2021-06-22 17:23:12 +0800

    update

--- init.lua
+++ init.lua
@@ -10,48 +10,48 @@ packer.startup(
             'nanotee/zoxide.vim',
             vim.api.nvim_set_keymap('n', '<space>z', ':Zi<CR>', { noremap = true, silent = true })
         }
-        use {
-            'sindrets/diffview.nvim',
-            config = function()
-                local cb = require'diffview.config'.diffview_callback
+        -- use {
+        --     'sindrets/diffview.nvim',
+        --     config = function()
+        --         local cb = require'diffview.config'.diffview_callback
 
-                require'diffview'.setup {
-                  diff_binaries = false,    -- Show diffs for binaries
-                  file_panel = {
-                    width = 35,
-                    use_icons = true        -- Requires nvim-web-devicons
-                  },
-                  key_bindings = {
-                    disable_defaults = false,                   -- Disable the default key bindings
-                    -- The `view` bindings are active in the diff buffers, only when the current
-                    -- tabpage is a Diffview.
-                    view = {
-                      ["<tab>"]     = cb("select_next_entry"),  -- Open the diff for the next file 
-                      ["<s-tab>"]   = cb("select_prev_entry"),  -- Open the diff for the previous file
-                      ["<leader>e"] = cb("focus_files"),        -- Bring focus to the files panel
-                      ["<leader>b"] = cb("toggle_files"),       -- Toggle the files panel.
-                    },
-                    file_panel = {
-                      ["j"]             = cb("next_entry"),         -- Bring the cursor to the next file entry
-                      ["<down>"]        = cb("next_entry"),
-                      ["k"]             = cb("prev_entry"),         -- Bring the cursor to the previous file entry.
-                      ["<up>"]          = cb("prev_entry"),
-                      ["<cr>"]          = cb("select_entry"),       -- Open the diff for the selected entry.
-                      ["o"]             = cb("select_entry"),
-                      ["<2-LeftMouse>"] = cb("select_entry"),
-                      ["-"]             = cb("toggle_stage_entry"), -- Stage / unstage the selected entry.
-                      ["S"]             = cb("stage_all"),          -- Stage all entries.
-                      ["U"]             = cb("unstage_all"),        -- Unstage all entries.
-                      ["R"]             = cb("refresh_files"),      -- Update stats and entries in the file list.
-                      ["<tab>"]         = cb("select_next_entry"),
-                      ["<s-tab>"]       = cb("select_prev_entry"),
-                      ["<leader>e"]     = cb("focus_files"),
-                      ["<leader>b"]     = cb("toggle_files"),
-                    }
-                  }
-                }
-            end
-        }
+        --         require'diffview'.setup {
+        --           diff_binaries = false,    -- Show diffs for binaries
+        --           file_panel = {
+        --             width = 35,
+        --             use_icons = true        -- Requires nvim-web-devicons
+        --           },
+        --           key_bindings = {
+        --             disable_defaults = false,                   -- Disable the default key bindings
+        --             -- The `view` bindings are active in the diff buffers, only when the current
+        --             -- tabpage is a Diffview.
+        --             view = {
+        --               ["<tab>"]     = cb("select_next_entry"),  -- Open the diff for the next file 
+        --               ["<s-tab>"]   = cb("select_prev_entry"),  -- Open the diff for the previous file
+        --               ["<leader>e"] = cb("focus_files"),        -- Bring focus to the files panel
+        --               ["<leader>b"] = cb("toggle_files"),       -- Toggle the files panel.
+        --             },
+        --             file_panel = {
+        --               ["j"]             = cb("next_entry"),         -- Bring the cursor to the next file entry
+        --               ["<down>"]        = cb("next_entry"),
+        --               ["k"]             = cb("prev_entry"),         -- Bring the cursor to the previous file entry.
+        --               ["<up>"]          = cb("prev_entry"),
+        --               ["<cr>"]          = cb("select_entry"),       -- Open the diff for the selected entry.
+        --               ["o"]             = cb("select_entry"),
+        --               ["<2-LeftMouse>"] = cb("select_entry"),
+        --               ["-"]             = cb("toggle_stage_entry"), -- Stage / unstage the selected entry.
+        --               ["S"]             = cb("stage_all"),          -- Stage all entries.
+        --               ["U"]             = cb("unstage_all"),        -- Unstage all entries.
+        --               ["R"]             = cb("refresh_files"),      -- Update stats and entries in the file list.
+        --               ["<tab>"]         = cb("select_next_entry"),
+        --               ["<s-tab>"]       = cb("select_prev_entry"),
+        --               ["<leader>e"]     = cb("focus_files"),
+        --               ["<leader>b"]     = cb("toggle_files"),
+        --             }
+        --           }
+        --         }
+        --     end
+        -- }
         use {
             'junegunn/fzf.vim',
             config = function()
@@ -267,21 +267,21 @@ packer.startup(
         }
         use {'tbodt/deoplete-tabnine', run = './install.sh'}
         use {'tpope/vim-fugitive'}
-        use {
-            'TimUntersberger/neogit',
-            config = function()
-                local neogit = require('neogit')
-                neogit.setup {
-                    integrations = {
-                        -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs you can use `sindrets/diffview.nvim`.
-                        -- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
-                        diffview = true
-                    }
-                }
-                vim.api.nvim_set_keymap('n', '<space>g', ':Neogit<CR>', { noremap = true, silent = true })
-            end,
-            requires = { 'nvim-lua/plenary.nvim', 'sindrets/diffview.nvim' }
-        }
+        -- use {
+        --     'TimUntersberger/neogit',
+        --     config = function()
+        --         local neogit = require('neogit')
+        --         neogit.setup {
+        --             integrations = {
+        --                 -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs you can use `sindrets/diffview.nvim`.
+        --                 -- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
+        --                 diffview = true
+        --             }
+        --         }
+        --         vim.api.nvim_set_keymap('n', '<space>g', ':Neogit<CR>', { noremap = true, silent = true })
+        --     end,
+        --     requires = { 'nvim-lua/plenary.nvim', 'sindrets/diffview.nvim' }
+        -- }
         use {'tpope/vim-unimpaired'}
         use {
             'RRethy/vim-illuminate',
